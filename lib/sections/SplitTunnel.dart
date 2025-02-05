1f3bf26f7b2d9d72600d4b59e275fa69097324fc
awesomebrownies <evan.n.brown25@gmail.com>
2025-02-05 09:18:04 -0500

    Add direct tunnel context

--- lib/sections/SplitTunnel.dart
+++ lib/sections/SplitTunnel.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:wirebird/DottedWireless.dart';
 
 import '../ConnectionButton.dart';
 import '../Selectable.dart';
 import '../SolidWire.dart';
 
-class SplitTunnel extends StatelessWidget{
+class SplitTunnel extends StatelessWidget {
   final bool connected;
   final VoidCallback toggleConnection;
   final Function setSelected;
   final String? inspectorSelection;
 
-  const SplitTunnel({super.key,
+  const SplitTunnel({
+    super.key,
     required this.connected,
     required this.toggleConnection,
     required this.setSelected,
@@ -25,14 +28,29 @@ class SplitTunnel extends StatelessWidget{
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            const SolidWire(width: 3, height: 2, activated: true),
             SizedBox(
-              height: 50,
+              height: 40,
+              child: Image.asset(
+                'assets/images/skip.png',
+              ),
+            ),
+            SizedBox(
+              width: screenWidth * 0.75 / 2 - 100,
+            ),
+            !connected ? AnimatedLineConnector(
+              distance: screenWidth * 0.75 / 2 - 100,
+              angle: 3.14,
+              color: Colors.black38,
+              spacing: 20.0,
+              dotSize: 1.0,
+            ) : const SizedBox.shrink(),
+            SizedBox(
+              height: 85,
               child: Image.asset(
                 connected ? 'assets/images/path_divider_turn.png' : 'assets/images/path_divider_straight.png',
               ),
             ),
-            SolidWire(width: (screenWidth*0.75/2 - 70), activated: true),
+            SolidWire(width: (screenWidth * 0.75 / 2 - 70), activated: true),
             SizedBox(
               height: 20,
               child: Image.asset('assets/images/o.png'),
@@ -44,8 +62,18 @@ class SplitTunnel extends StatelessWidget{
           children: [
             ConnectionButton(connected: connected, toggleConnection: toggleConnection),
             Padding(
-              padding: const EdgeInsets.only(left: 35.0, right: 95.0),
-              child: SolidWire(width: 3, height: 110, activated: connected),
+              padding: const EdgeInsets.only(left: 23, right: 75.5),
+              child: SizedBox(
+                    width: 75,
+                    height: 0,
+                    child: connected ? const AnimatedLineConnector(
+                                    distance: 110,
+                                    angle: 1.57079,
+                                    color: Colors.black38,
+                                    spacing: 20.0,
+                                    dotSize: 1.0,
+                                  ) : const SizedBox.shrink(),
+                  )// Empty widget when `connected` is false
             ),
             Selectable(
               imagePath: 'assets/images/control_center.png',
@@ -53,9 +81,9 @@ class SplitTunnel extends StatelessWidget{
               description: 'No Controls',
               inspectorSelection: inspectorSelection,
               onClick: () => setSelected("Control Center"),
-            )
+            ),
           ],
-        )
+        ),
       ],
     );
   }
